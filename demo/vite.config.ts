3cf46ac022098c7449c5dad8c37909a30644e55b
Axel Meinhardt <26243798+ameinhardt@users.noreply.github.com>
2024-12-02 16:16:08 +0100

    fix: allow demo BASE_URL
    Signed-off-by: Axel Meinhardt <26243798+ameinhardt@users.noreply.github.com>

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,7 +1,9 @@
+import process from 'node:process';
 import unocss from 'unocss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.BASE_URL ?? '/',
   plugins: [
     unocss()
   ]
